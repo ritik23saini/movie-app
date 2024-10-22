d37348900b097395808387e22e0c39fc558a0128
RITIK-SAINI23 <ritik23saini@gmail.com>
2024-10-23 00:23:54 +0530

    frontend bug fixed

---
+++ frontend/src/redux/api/movies.js
@@ -0,0 +1,91 @@
+import { apiSlice } from "./apiSlice";
+import { MOVIE_URL, UPLOAD_URL } from "../constants";
+
+export const moviesApiSlice = apiSlice.injectEndpoints({
+  endpoints: (builder) => ({
+    getAllMovies: builder.query({
+      query: () => `${MOVIE_URL}/all-movies`,
+      providesTags: ['Movies'],
+    }),
+    createMovie: builder.mutation({
+      query: (newMovie) => ({
+        url: `${MOVIE_URL}/create-movie`,
+        method: "POST",
+        body: newMovie,
+        invalidatesTags: ['Movies'],
+      }),
+    }),
+
+    updateMovie: builder.mutation({
+      query: ({ id, updatedMovie }) => ({
+        url: `${MOVIE_URL}/update-movie/${id}`,
+        method: "PUT",
+        body: updatedMovie,
+        invalidatesTags: ['Movies'],
+      }),
+    }),
+
+    addMovieReview: builder.mutation({
+      query: ({ id, rating, comment }) => ({
+        url: `${MOVIE_URL}/${id}/reviews`,
+        method: "POST",
+        body: { rating, id, comment },
+      }),
+    }),
+
+    deleteComment: builder.mutation({
+      query: ({ movieId, reviewId }) => ({
+        url: `${MOVIE_URL}/delete-comment`,
+        method: "DELETE",
+        body: { movieId, reviewId },
+      }),
+    }),
+
+    deleteMovie: builder.mutation({
+      query: (id) => ({
+        url: `${MOVIE_URL}/delete-movie/${id}`,
+        method: "DELETE",
+        invalidatesTags: ['Movies'],
+      }),
+    }),
+
+    getSpecificMovie: builder.query({
+      query: (id) => `${MOVIE_URL}/specific-movie/${id}`,
+    }),
+
+    uploadImage: builder.mutation({
+      query: (formData) => ({
+        url: `${UPLOAD_URL}`,
+        method: "POST",
+        body: formData,
+      }),
+    }),
+
+    getNewMovies: builder.query({
+      query: () => `${MOVIE_URL}/new-movies`,
+    }),
+
+    getTopMovies: builder.query({
+      query: () => `${MOVIE_URL}/top-movies`,
+    }),
+
+    getRandomMovies: builder.query({
+      query: () => `${MOVIE_URL}/random-movies`,
+    }),
+  }),
+});
+
+export const {
+  useGetAllMoviesQuery,
+  useCreateMovieMutation,
+  useUpdateMovieMutation,
+  useAddMovieReviewMutation,
+  useDeleteCommentMutation,
+  useGetSpecificMovieQuery,
+  useUploadImageMutation,
+  useDeleteMovieMutation,
+  //
+  useGetNewMoviesQuery,
+  useGetTopMoviesQuery,
+  useGetRandomMoviesQuery,
+} = moviesApiSlice;
